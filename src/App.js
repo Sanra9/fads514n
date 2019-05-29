e0b2e5678051596a3c2ee05aa980f7419e436d37
sanra9 <sanrra999@gmail.com>
2019-05-29 12:13:17 -0500

    Jé

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ class App extends Component {
     }
   }
   render() {
+    const { list } = this.state
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+        {list.map((list) => <li>{list}</li>)}
         </ul>
       </div>
     );
